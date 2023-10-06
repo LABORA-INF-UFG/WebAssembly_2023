ba77d4917c74e1972043df1f05b1684f9254a1ea
xxGabrielNeryxx <gabrielgildino@gmail.com>
2023-10-06 09:41:07 -0300

    Criação de um novo arquivo mjs

---
+++ AlvaAR/ALVAar_node/main.mjs
@@ -0,0 +1,4 @@
+//const {Stats} = require("./assets/stats.js");
+import {AlvaAR} from "./assets/alva_ar.js";
+import {ARSimpleView, ARSimpleMap} from "./assets/view.js";
+import {Video, onFrame} from "./assets/utils.js";
