7714c43f631a52834eb05eadb30fbea5930738f1
Matheus Lucas <nerdola.cartola@gmail.com>
2023-10-10 19:53:12 -0300

    Reading video from assets

--- server-alvaar/app.js
+++ server-alvaar/app.js
@@ -1,16 +1,23 @@
 
 import { AlvaAR } from './assets/alva_ar.js';
 import express from 'express'
+import fs from 'fs'
+
 const app = express()
 const port = 3000
+const videoPath = './assets/video.mp4';
 
 app.get('/', async (req, res) => {
+  const stat = fs.statSync(videoPath);
   const alva = await AlvaAR.Initialize( 300, 300 );
 
   console.log(alva)
-  console.log(Object.keys(alva))
 
-  res.send('Hello World!')
+  const responseObject = {
+    stat
+  }  
+
+  res.send(JSON.stringify(responseObject))
 })
 
 app.listen(port, () => {
