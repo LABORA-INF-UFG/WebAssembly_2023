ded7d619007c10ae6771b070c3010b534261f294
Matheus Lucas <nerdola.cartola@gmail.com>
2023-10-11 17:06:51 -0300

    Adding cors policy

--- server-alvaar/src/app.ts
+++ server-alvaar/src/app.ts
@@ -1,35 +1,39 @@
 // @ts-ignore
 import { AlvaAR } from '../libraries/alva_ar.js';
 import express from 'express'
-import fs from 'fs'
+import cors from 'cors';
 
 const app = express()
 app.use(express.json());
+app.use(cors())
 const port = 3000
 const videoPath = 'assets/video.mp4';
 
+// TODO: Get dimensions dynamically
+const width = 364;
+const height = 674;
+
+let alva: AlvaAR | null = null;
+
 app.get('/', async (req, res) => {
-  res.send(JSON.stringify({
+  res.json({
     message: "OK",
     status: true
-  }))
+  })
+})
+
+app.post('/initialize', async (req, res) => {
+  alva = await AlvaAR.Initialize(width, height);
+
+  res.send("AlvaAR initialized")
 })
 
 app.post('/video', async (req, res) => {
-  const stat = fs.statSync(videoPath);
+  console.log(req.body)
 
-  // TODO: Get dimensions dynamically
-  const width = 364;
-  const height = 674;
-
-  const alva: AlvaAR = await AlvaAR.Initialize(width, height);
-
-  // const pose = alva.findCameraPose( frame );
-
-  res.send(JSON.stringify(req.body))
+  res.json(req.body)
 })
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
-
