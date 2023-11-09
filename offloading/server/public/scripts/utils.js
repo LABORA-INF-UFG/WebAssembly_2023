f36b55ae426ae7212743bf2ebff2eb381ef5de57
Matheus Lucas <nerdola.cartola@gmail.com>
2023-11-09 20:00:21 -0300

    Saving stars in local version

--- offloading/server/public/scripts/utils.js
+++ offloading/server/public/scripts/utils.js
@@ -1,65 +1,91 @@
 const rad2deg = 180.0 / Math.PI;
 const deg2rad = Math.PI / 180;
 
-function onFrame( frameTickFn, fps = 30)
-{
+function getCSV(statistics, fileName) {
+    let csvContent = statistics.map(e => e.join(",")).join("\n");
+    let blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    let link = document.createElement("a");
+
+    link.href = URL.createObjectURL(blob);
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+function roughSizeOfObject(object) {
+    const objectList = [];
+    const stack = [object];
+    const bytes = [0];
+    while (stack.length) {
+        const value = stack.pop();
+        if (value == null) bytes[0] += 4;
+        else if (typeof value === 'boolean') bytes[0] += 4;
+        else if (typeof value === 'string') bytes[0] += value.length * 2;
+        else if (typeof value === 'number') bytes[0] += 8;
+        else if (typeof value === 'object' && objectList.indexOf(value) === -1) {
+            objectList.push(value);
+            if (typeof value.byteLength === 'number') bytes[0] += value.byteLength;
+            else if (value[Symbol.iterator]) {
+                // eslint-disable-next-line no-restricted-syntax
+                for (const v of value) stack.push(v);
+            } else {
+                Object.keys(value).forEach(k => {
+                    bytes[0] += k.length * 2; stack.push(value[k]);
+                });
+            }
+        }
+    }
+    return bytes[0];
+}
+
+function onFrame(frameTickFn, fps = 30) {
     const fpsInterval = ~~(1000 / fps);
 
     let t1 = performance.now();
 
-    const onAnimationFrame = async () =>
-    {
+    const onAnimationFrame = async () => {
         const t2 = performance.now();
         const td = t2 - t1;
 
-        if( td > fpsInterval )
-        {
+        if (td > fpsInterval) {
             t1 = t2 - (td % fpsInterval);
 
-            if( (await frameTickFn( t2 )) === false )
-            {
+            if ((await frameTickFn(t2)) === false) {
                 return;
             }
         }
 
-        requestAnimationFrame( onAnimationFrame );
+        requestAnimationFrame(onAnimationFrame);
     };
 
-    requestAnimationFrame( onAnimationFrame );
+    requestAnimationFrame(onAnimationFrame);
 }
 
-function isIOS()
-{
-    return /iPad|iPhone|iPod/.test( navigator.platform );
+function isIOS() {
+    return /iPad|iPhone|iPod/.test(navigator.platform);
 }
 
-function isMobile()
-{
-    try
-    {
-        document.createEvent( 'TouchEvent' );
+function isMobile() {
+    try {
+        document.createEvent('TouchEvent');
         return true;
-    } catch( e )
-    {
+    } catch (e) {
         return false;
     }
 }
 
-function getScreenOrientation()
-{
+function getScreenOrientation() {
     let angle = -1;
 
-    if( window.screen && window.screen.orientation )
-    {
+    if (window.screen && window.screen.orientation) {
         angle = window.screen.orientation.angle;
     }
-    else if( 'orientation' in window )
-    {
+    else if ('orientation' in window) {
         angle = window.orientation;
     }
 
-    switch( angle )
-    {
+    switch (angle) {
         case 0:
             return 'portrait';
         case 90:
@@ -75,20 +101,17 @@ function getScreenOrientation()
     return 'unknown';
 }
 
-function resize2cover( srcW, srcH, dstW, dstH )
-{
+function resize2cover(srcW, srcH, dstW, dstH) {
     const rect = {};
 
-    if( dstW / dstH > srcW / srcH )
-    {
+    if (dstW / dstH > srcW / srcH) {
         const scale = dstW / srcW;
         rect.width = ~~(scale * srcW);
         rect.height = ~~(scale * srcH);
         rect.x = 0;
         rect.y = ~~((dstH - rect.height) * 0.5);
     }
-    else
-    {
+    else {
         const scale = dstH / srcH;
         rect.width = ~~(scale * srcW);
         rect.height = ~~(scale * srcH);
@@ -99,9 +122,8 @@ function resize2cover( srcW, srcH, dstW, dstH )
     return rect;
 }
 
-function createCanvas( width, height )
-{
-    const canvas = document.createElement( 'canvas' );
+function createCanvas(width, height) {
+    const canvas = document.createElement('canvas');
 
     canvas.width = width;
     canvas.height = height;
@@ -109,43 +131,33 @@ function createCanvas( width, height )
     return canvas;
 }
 
-class Camera
-{
-    static async Initialize( constraints = null )
-    {
-        if( 'facingMode' in constraints && 'deviceId' in constraints )
-        {
-            throw new Error( `Camera settings 'deviceId' and 'facingMode' are mutually exclusive.` );
+class Camera {
+    static async Initialize(constraints = null) {
+        if ('facingMode' in constraints && 'deviceId' in constraints) {
+            throw new Error(`Camera settings 'deviceId' and 'facingMode' are mutually exclusive.`);
         }
 
-        if( 'facingMode' in constraints && ['environment', 'user'].indexOf( constraints.facingMode ) === -1 )
-        {
-            throw new Error( `Camera settings 'facingMode' can only be 'environment' or 'user'.` );
+        if ('facingMode' in constraints && ['environment', 'user'].indexOf(constraints.facingMode) === -1) {
+            throw new Error(`Camera settings 'facingMode' can only be 'environment' or 'user'.`);
         }
 
-        const setupUserMediaStream = ( permission ) =>
-        {
-            return new Promise( ( resolve, reject ) =>
-            {
-                const onSuccess = ( stream ) =>
-                {
+        const setupUserMediaStream = (permission) => {
+            return new Promise((resolve, reject) => {
+                const onSuccess = (stream) => {
                     const track = stream.getVideoTracks()[0];
 
-                    if( typeof track === 'undefined' )
-                    {
-                        reject( new Error( `Failed to access camera: Permission denied (No track).` ) );
+                    if (typeof track === 'undefined') {
+                        reject(new Error(`Failed to access camera: Permission denied (No track).`));
                     }
-                    else
-                    {
-                        const video = document.createElement( 'video' );
+                    else {
+                        const video = document.createElement('video');
 
-                        video.setAttribute( 'autoplay', 'autoplay' );
-                        video.setAttribute( 'playsinline', 'playsinline' );
-                        video.setAttribute( 'webkit-playsinline', 'webkit-playsinline' );
+                        video.setAttribute('autoplay', 'autoplay');
+                        video.setAttribute('playsinline', 'playsinline');
+                        video.setAttribute('webkit-playsinline', 'webkit-playsinline');
                         video.srcObject = stream;
 
-                        video.onloadedmetadata = () =>
-                        {
+                        video.onloadedmetadata = () => {
                             const settings = track.getSettings();
 
                             const tw = settings.width;
@@ -153,9 +165,8 @@ class Camera
                             const vw = video.videoWidth;
                             const vh = video.videoHeight;
 
-                            if( vw !== tw || vh !== th )
-                            {
-                                console.warn( `Video dimensions mismatch: width: ${ tw }/${ vw }, height: ${ th }/${ vh }` );
+                            if (vw !== tw || vh !== th) {
+                                console.warn(`Video dimensions mismatch: width: ${tw}/${vw}, height: ${th}/${vh}`);
                             }
 
                             video.style.width = vw + 'px';
@@ -164,182 +175,157 @@ class Camera
                             video.height = vh;
                             video.play();
 
-                            resolve( new Camera( video ) );
+                            resolve(new Camera(video));
                         };
                     }
                 };
 
-                const onFailure = ( error ) =>
-                {
-                    switch( error.name )
-                    {
+                const onFailure = (error) => {
+                    switch (error.name) {
                         case 'NotFoundError':
                         case 'DevicesNotFoundError':
-                            reject( new Error( `Failed to access camera: Camera not found.` ) );
+                            reject(new Error(`Failed to access camera: Camera not found.`));
                             return;
                         case 'SourceUnavailableError':
-                            reject( new Error( `Failed to access camera: Camera busy.` ) );
+                            reject(new Error(`Failed to access camera: Camera busy.`));
                             return;
                         case 'PermissionDeniedError':
                         case 'SecurityError':
-                            reject( new Error( `Failed to access camera: Permission denied.` ) );
+                            reject(new Error(`Failed to access camera: Permission denied.`));
                             return;
                         default:
-                            reject( new Error( `Failed to access camera: Rejected.` ) );
+                            reject(new Error(`Failed to access camera: Rejected.`));
                             return;
                     }
                 };
 
-                if( permission && permission.state === 'denied' )
-                {
-                    reject( new Error( `Failed to access camera: Permission denied.` ) );
+                if (permission && permission.state === 'denied') {
+                    reject(new Error(`Failed to access camera: Permission denied.`));
                     return;
                 }
 
-                navigator.mediaDevices.getUserMedia( constraints ).then( onSuccess ).catch( onFailure );
-            } );
+                navigator.mediaDevices.getUserMedia(constraints).then(onSuccess).catch(onFailure);
+            });
         };
 
-        if( navigator.permissions && navigator.permissions.query )
-        {
+        if (navigator.permissions && navigator.permissions.query) {
             return navigator.permissions
-            .query( { name: 'camera' } )
-            .then( ( permission ) =>
-            {
-                return setupUserMediaStream( permission );
-            } )
-            .catch( error =>
-            {
-                return setupUserMediaStream();
-            } );
+                .query({ name: 'camera' })
+                .then((permission) => {
+                    return setupUserMediaStream(permission);
+                })
+                .catch(error => {
+                    return setupUserMediaStream();
+                });
         }
-        else
-        {
+        else {
             return setupUserMediaStream();
         }
     }
 
-    constructor( videoElement )
-    {
+    constructor(videoElement) {
         this.el = videoElement;
         this.width = videoElement.videoWidth;
         this.height = videoElement.videoHeight;
 
-        this._canvas = createCanvas( this.width, this.height );
-        this._ctx = this._canvas.getContext( '2d', { willReadFrequently: true } );
+        this._canvas = createCanvas(this.width, this.height);
+        this._ctx = this._canvas.getContext('2d', { willReadFrequently: true });
     }
 
-    getImageData()
-    {
-        this._ctx.clearRect( 0, 0, this.width, this.height );
-        this._ctx.drawImage( this.el, 0, 0, this.width, this.height );
+    getImageData() {
+        this._ctx.clearRect(0, 0, this.width, this.height);
+        this._ctx.drawImage(this.el, 0, 0, this.width, this.height);
 
-        return this._ctx.getImageData( 0, 0, this.width, this.height );
+        return this._ctx.getImageData(0, 0, this.width, this.height);
     }
 }
 
-class Video
-{
-    static async Initialize( url, timeout = 8000 )
-    {
-        return new Promise( ( resolve, reject ) =>
-        {
+class Video {
+    static async Initialize(url, timeout = 8000) {
+        return new Promise((resolve, reject) => {
             let tid = -1;
 
-            const video = document.createElement( 'video' );
+            const video = document.createElement('video');
 
             video.src = url;
-            video.setAttribute( 'autoplay', 'autoplay' );
-            video.setAttribute( 'playsinline', 'playsinline' );
-            video.setAttribute( 'webkit-playsinline', 'webkit-playsinline' );
+            video.setAttribute('autoplay', 'autoplay');
+            video.setAttribute('playsinline', 'playsinline');
+            video.setAttribute('webkit-playsinline', 'webkit-playsinline');
             video.autoplay = true;
             video.muted = true;
             video.loop = true; // note: if loop is true, ended event will not fire
             video.load();
 
-            tid = setTimeout( () =>
-            {
-                reject( new Error( `Failed to load video: Timed out after ${ timeout }ms.` ) );
-            }, timeout );
+            tid = setTimeout(() => {
+                reject(new Error(`Failed to load video: Timed out after ${timeout}ms.`));
+            }, timeout);
 
-            video.onerror = () =>
-            {
-                clearTimeout( tid );
+            video.onerror = () => {
+                clearTimeout(tid);
 
-                reject( new Error( `Failed to load video.` ) );
+                reject(new Error(`Failed to load video.`));
             };
 
-            video.onabort = () =>
-            {
-                clearTimeout( tid );
+            video.onabort = () => {
+                clearTimeout(tid);
 
-                reject( new Error( `Failed to load video: Load aborted.` ) );
+                reject(new Error(`Failed to load video: Load aborted.`));
             };
 
-            if( video.readyState >= 4 )
-            {
-                clearTimeout( tid );
+            if (video.readyState >= 4) {
+                clearTimeout(tid);
 
-                resolve( video );
+                resolve(video);
             }
-            else
-            {
-                video.oncanplaythrough = () =>
-                {
-                    clearTimeout( tid );
+            else {
+                video.oncanplaythrough = () => {
+                    clearTimeout(tid);
 
-                    if( video.videoWidth === 0 || video.videoHeight === 0 )
-                    {
-                        reject( new Error( `Failed to load video: Invalid dimensions.` ) );
+                    if (video.videoWidth === 0 || video.videoHeight === 0) {
+                        reject(new Error(`Failed to load video: Invalid dimensions.`));
                     }
-                    else
-                    {
-                        resolve( video );
+                    else {
+                        resolve(video);
                     }
                 };
             }
-        } ).then( video =>
-        {
+        }).then(video => {
             video.onload = video.onabort = video.onerror = null;
 
-            return new Video( video );
-        } );
+            return new Video(video);
+        });
     }
 
-    constructor( videoElement )
-    {
+    constructor(videoElement) {
         this.el = videoElement;
         this.width = videoElement.videoWidth;
         this.height = videoElement.videoHeight;
 
-        this._canvas = createCanvas( this.width, this.height );
-        this._ctx = this._canvas.getContext( '2d', { willReadFrequently: true } );
+        this._canvas = createCanvas(this.width, this.height);
+        this._ctx = this._canvas.getContext('2d', { willReadFrequently: true });
 
         this._lastTime = -1;
         this._imageData = null;
     }
 
-    getImageData()
-    {
+    getImageData() {
         const t = this.el.currentTime;
 
-        if( this._lastTime !== t )
-        {
+        if (this._lastTime !== t) {
             this._lastTime = t;
 
             this._imageData = null;
         }
 
-        if( this._imageData === null )
-        {
-            this._ctx.clearRect( 0, 0, this.width, this.height );
-            this._ctx.drawImage( this.el, 0, 0, this.width, this.height );
+        if (this._imageData === null) {
+            this._ctx.clearRect(0, 0, this.width, this.height);
+            this._ctx.drawImage(this.el, 0, 0, this.width, this.height);
 
-            this._imageData = this._ctx.getImageData( 0, 0, this.width, this.height );
+            this._imageData = this._ctx.getImageData(0, 0, this.width, this.height);
         }
 
         return this._imageData;
     }
 }
 
-export { Camera, Video, onFrame, isMobile, isIOS, getScreenOrientation, resize2cover, rad2deg, deg2rad }
+export { Camera, Video, onFrame, isMobile, isIOS, getScreenOrientation, resize2cover, rad2deg, deg2rad, getCSV, roughSizeOfObject }
