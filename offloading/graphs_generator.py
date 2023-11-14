31dcf8c74e4bc3fea100f419eb8e8e929aa7303e
xxGabrielNeryxx <gabrielgildino@gmail.com>
2023-11-14 20:01:39 -0300

    graficos melhorados

--- offloading/graphs_generator.py
+++ offloading/graphs_generator.py
@@ -3,62 +3,85 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
+def gen_local_statistics(local_statistic_path, labels):
 
-folder_path = './data'
-
-def genGraph(statistic): 
-  statistic_path = os.path.join(folder_path, statistic)
-  mean = {}
-  labels = []
-  
-  for case in os.listdir(statistic_path):
-    case_path = os.path.join(statistic_path, case)
     arr = []
     
-    for filename in os.listdir(case_path):
-      if filename.endswith('.csv'):  
-          file_path = os.path.join(case_path, filename)
-
-          if os.path.isfile(file_path):
-            df = pd.read_csv(file_path)      
-            arr.append(df)
+    for filename in os.listdir(local_statistic_path):
+        df = pd.DataFrame(columns= labels)
+      
+        if filename.endswith('.csv'):
+            file_path = os.path.join(local_statistic_path, filename)
+            df = pd.read_csv(file_path)    
+        
+        arr.append(df[labels])        
+    try:
+        statistics = pd.concat(arr, ignore_index=True).mean().to_frame().transpose()
+    except:
+        statistics = pd.DataFrame(columns=labels)
     
-    data = pd.concat(arr, ignore_index=True)
-    data.drop(columns=['clientMemoryUsage', 'serverMemoryUsage', 'requestSize', 'responseSize'], inplace=True)
-    labels = data.columns
-    
-    mean[float(case)] = data.mean().to_frame().transpose()
- 
-  mean = dict(sorted(mean.items()))  
-  
-  
-  fig, ax = plt.subplots()  
-  
-  for label in labels:
-    x = list(mean.keys())
-    y = [mean[key][label] for key in x]
-    ax.plot(x, y, marker='o', label=label)
-  
- 
+    statistics.columns =  statistics.columns.map(lambda x: x+'Local')
 
-# Set the format of the y-tick labels
-  ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.2f'))
-  
-  ax.legend()  
-  
-for statistic in os.listdir(folder_path):
-  genGraph(statistic)
-  if statistic == 'bandwidth':
-    plt.xlabel('Bandwidth(Mbps)')
-  elif statistic == 'delay':
-    plt.xlabel('Delay(ms)')
-  elif statistic == 'packetloss':
-    plt.xlabel('Packet loss(%)')
-  
-  plt.ylabel('Average Time(ms)')
-  plt.savefig(f'./graphs/plot_{statistic}.png')
-  
+    return statistics
+    
+    
+def gen_graphs(offloading_statistics_path, labels, local_statistics): 
+    mean = {}
+    auxLabel = []
+
+    for case in os.listdir(offloading_statistics_path):
+        case_path = os.path.join(offloading_statistics_path, case)
+        arr = []
+        
+        for filename in os.listdir(case_path):
+            df = pd.DataFrame(columns= labels)
+            
+            if filename.endswith('.csv'):
+                file_path = os.path.join(case_path, filename)
+                df = pd.read_csv(file_path)    
+                
+            arr.append(df[labels])
+
+        try:
+            data = pd.concat(arr, ignore_index=True).mean().to_frame().transpose()
+            
+        except:
+            data = pd.DataFrame(columns= labels)
+
+        
+        data = pd.concat([data, local_statistics], axis=1)
+        
+        auxLabel = data.columns
+        mean[float(case)] = data
     
         
+    mean = dict(sorted(mean.items()))  
+    
+    fig, ax = plt.subplots()  
+  
+    for label in auxLabel:
+        x = list(mean.keys())
+        y = [mean[key][label] for key in x]
+        ax.plot(x, y, marker='o', label=label)
+  
+    ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.2f'))
+  
+    ax.legend() 
     
 
+  
+def main():
+    local_statistic_path = './dataLocal'
+    local_labels = ['FPS','slamTime', 'renderTime', 'videoTime']
+    
+    offloading_statistic_path = './dataOffloading/delay'
+    offloading_labels = ['FPS','serverTime', 'slamTime', 'streamingTime', 'renderTime', 'videoTime']
+    
+    local_statistics = gen_local_statistics(local_statistic_path, local_labels)
+    gen_graphs(offloading_statistic_path, offloading_labels, local_statistics)
+    
+    plt.savefig(f'./graphsOffloading/test.png')
+  
+    
+if __name__ == "__main__":
+    main()
