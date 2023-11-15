3c511b253271d10ba5bcec5407262ffa8b17c07b
Matheus Lucas <nerdola.cartola@gmail.com>
2023-11-15 13:03:10 -0300

    Puppeteer test

---
+++ tests/puppeteer/test.js
@@ -0,0 +1,38 @@
+import puppeteer from 'puppeteer';
+
+async function sleep(time) {
+    await new Promise(resolve => setTimeout(resolve, time));
+}
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1080, height: 1024 });
+
+    // Expose a handler to the page
+    await page.exposeFunction('onCustomEvent', async ({ type }) => {
+        console.log(type);
+
+        if(type === "end") {
+            await page.screenshot({ path: 'screenshot.png' })
+            await sleep(1000);
+            await browser.close();
+        }
+    });
+
+    // listen for events of type 'status' and
+    // pass 'type' and 'detail' attributes to our exposed function
+    await page.evaluateOnNewDocument(() => {
+        window.addEventListener('start', ({ type }) => {
+            window.onCustomEvent({ type });
+        });
+    });
+
+    await page.evaluateOnNewDocument(() => {
+        window.addEventListener('end', ({ type }) => {
+            window.onCustomEvent({ type });
+        });
+    });
+
+    await page.goto('http://localhost:3000/local.html');
+})();
