b47c00b07e7d51bc1e4032ed3f7d8176a9e45c22
Matheus Lucas <nerdola.cartola@gmail.com>
2023-11-17 11:44:07 -0300

    Changing url for tests

--- tests/puppeteer/test.js
+++ tests/puppeteer/test.js
@@ -32,5 +32,5 @@ async function sleep(time) {
         });
     });
 
-    await page.goto('http://localhost:3000/offloading.html');
+    await page.goto('http://192.168.10.2:3000/offloading.html');
 })();
