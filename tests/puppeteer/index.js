f764aec4fb17eccedfa64e8d5825acfa001f0f7e
Matheus Lucas <nerdola.cartola@gmail.com>
2023-11-21 21:06:39 -0300

    Update experiment

--- tests/puppeteer/index.js
+++ tests/puppeteer/index.js
@@ -9,26 +9,14 @@ async function sleep(time) {
     const page = await browser.newPage();
     await page.setViewport({ width: 1080, height: 1024 });
 
-    // Expose a handler to the page
-    await page.exposeFunction('onCustomEvent', async ({ type }) => {
-        if(type === "end") {
-            // await page.screenshot({ path: 'screenshot.png' })
-            await sleep(1000);
-            await browser.close();
-        }
-    });
-
-    // listen for events of type 'status' and
-    // pass 'type' and 'detail' attributes to our exposed function
-    await page.evaluateOnNewDocument(() => {
-        window.addEventListener('start', ({ type }) => {
-            window.onCustomEvent({ type });
-        });
+    await page.exposeFunction('endExperiment', async ({ type }) => {
+        await sleep(1000);
+        await browser.close();
     });
 
     await page.evaluateOnNewDocument(() => {
-        window.addEventListener('end', ({ type }) => {
-            window.onCustomEvent({ type });
+        window.addEventListener('end', () => {
+            window.endExperiment();
         });
     });
 
