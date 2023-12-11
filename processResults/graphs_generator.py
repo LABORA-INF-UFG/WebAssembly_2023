ff2b1e275837d4f42fd63425058cee182552cbcb
xxGabrielNeryxx <gabrielgildino@gmail.com>
2023-12-10 21:28:27 -0300

    adding new graphs

--- processResults/graphs_generator.py
+++ processResults/graphs_generator.py
@@ -21,14 +21,17 @@ def gen_formated_data(offloading_statistics_path, labels):
                 file_path = os.path.join(case_path, filename)
                 df = pd.read_csv(file_path)    
                 
+                FPS = (df['totalTime'].count()*1000)/df['totalTime'].sum()
+
+                df = df.mean().to_frame().T
+                df['FPS'] = FPS
             arr.append(df[labels])
 
         try:
-        
+            
             data = pd.concat(arr, ignore_index=True).describe().T
             data = data[['mean', 'std', 'count']]
             interval = z * data['std']/np.sqrt(data['count'])
-
             data['upper'] = data['mean'] + interval
             data['lower'] = data['mean'] - interval
             data = data.T
@@ -42,9 +45,8 @@ def gen_formated_data(offloading_statistics_path, labels):
             case = case.replace('ms', '')
         elif case.find('Mbps') != -1:
             case = case.replace('Mbps', '')    
-            
         cases[float(case)] = data
-    
+        
         
     cases = dict(sorted(cases.items()))  
     
@@ -72,7 +74,6 @@ def gen_bar_plot(statistic, raw_data, case, legend):
         data[key] = data[key][list(legend.keys())]
         data[key].rename(columns = legend ,inplace = True)
         
-    
     bottom = np.zeros(len(data.keys()))
     
     for label in legend.values():
@@ -93,12 +94,11 @@ def gen_bar_plot(statistic, raw_data, case, legend):
     plt.savefig(f'./graphs/{statistic}_{case}.png')
 
 
-def gen_pair_graphs(pair, offloading_data, local_data, statistic):
-    data = {}
+def gen_pair_graphs(graph, offloading_data, local_data, statistic):
     fig, ax = plt.subplots(figsize=(13, 6))  
     ax.grid(axis='y')
 
-    if pair[0] == 'FPSOffloading':
+    if graph == 'FPS':
         plt.ylabel('FPS')
     else:
         plt.ylabel('Time(ms)')
@@ -113,70 +113,82 @@ def gen_pair_graphs(pair, offloading_data, local_data, statistic):
         plt.xlabel("Packetloss(%)")
         horizontal_line_width=0.05
     
-    for key in offloading_data.keys():    
-        data[key] = offloading_data[key].copy(deep= True)
-        data[key] = pd.concat([data[key], local_data[key]], axis=1)
-        data[key] = data[key][pair]
-        data[key] = data[key].T.drop_duplicates().T    
-    
     arr_max_top = []
     
-    for label in pair:
-        
-        if label.find('Local') == -1:
-            color = '#ff8c00'
-            legend = 'Offloading'
-        else:
-            color = '#2b35af'
-            legend = 'Local'
-       
-        x = data.keys()
-        y = [data[key][label]['mean'] for key in x]
-       
-        ax.plot(x, y, marker='o', label=legend, color = color, linestyle='--')       
-        plt.xticks(list(data.keys()))
-        
-        for key in data.keys():
-            left = key - horizontal_line_width/2
-            right = key + horizontal_line_width/2
-            bottom = data[key][label]['lower']
-            top = data[key][label]['upper']
-            plt.plot([key, key], [top, bottom], color = color)
-            plt.plot([left, right], [top, top], color = color)
-            plt.plot([left, right], [bottom, bottom], color = color)       
+    offloading_color = '#ff8c00'
+    legend_offloading = 'Offloading'
+    
+    x_offloading = offloading_data.keys()
+    y_offloading = [offloading_data[key][graph]['mean'] for key in x_offloading]
+    
+    ax.plot(x_offloading, y_offloading, marker='o', label=legend_offloading, color = offloading_color, linestyle='--')       
+    plt.xticks(list(x_offloading))
+    
+    for key in x_offloading:
+        left = key - horizontal_line_width/2
+        right = key + horizontal_line_width/2
+        bottom = offloading_data[key][graph]['lower']
+        top = offloading_data[key][graph]['upper']
+        plt.plot([key, key], [top, bottom], color = offloading_color)
+        plt.plot([left, right], [top, top], color = offloading_color)
+        plt.plot([left, right], [bottom, bottom], color = offloading_color)       
 
-            arr_max_top.append(0 if math.isnan(top) else top)
+        arr_max_top.append(0 if math.isnan(top) else top)
 
+    
+    local_color = '#2b35af'
+    local_legend = 'Local'
+    
+    x_local = local_data.keys()
+    y_local = [local_data[key][graph]['mean'] for key in x_local]
+    
+    ax.plot(x_local, y_local, marker='o', label=local_legend, color = local_color, linestyle='--')       
+    plt.xticks(list(x_local))
+    
+    for key in x_local:
+        left = key - horizontal_line_width/2
+        right = key + horizontal_line_width/2
+        bottom = local_data[key][graph]['lower']
+        top = local_data[key][graph]['upper']
+        plt.plot([key, key], [top, bottom], color = local_color)
+        plt.plot([left, right], [top, top], color = local_color)
+        plt.plot([left, right], [bottom, bottom], color = local_color)       
+
+        arr_max_top.append(0 if math.isnan(top) else top)
+
+    
     plt.ylim([0, max(arr_max_top) + 0.5])    
     ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.2f'))
     
     ax.legend() 
      
    
-    if pair[0] == 'FPSOffloading':
+    if graph == 'FPS':
         plt.savefig(f'./graphs/{statistic}_FPS.png')
-    elif pair[0] == 'serverTimeOffloading':
+    elif graph == 'slamTime':
         plt.savefig(f'./graphs/{statistic}_slam_time.png')
-    elif pair[0] == 'renderTimeOffloading':
+    elif graph == 'renderTime':
         plt.savefig(f'./graphs/{statistic}_render_time.png')
-    elif pair[0] == 'videoTimeOffloading':
+    elif graph == 'segmentationTime':
         plt.savefig(f'./graphs/{statistic}_video_time.png')
         
 
 def main():
     statistics = ['rate', 'loss', 'delay']
-
+    
     local_path = './dataLocal'
-    local_labels = ['FPSLocal','slamTimeLocal', 'renderTimeLocal', 'videoTimeLocal']
+    local_labels = ['totalTime','slamTime', 'renderTime', 'segmentationTime','FPS']
     
+
+
     offloading_path = './dataOffloading'
-    offloading_labels = ['FPSOffloading','serverTimeOffloading', 'slamTimeOffloading', 'streamingTimeOffloading', 'renderTimeOffloading', 'videoTimeOffloading']
+    offloading_labels = ['totalTime', 'slamTime', 'networkTime', 'renderTime', 'segmentationTime', 'FPS']
     
-    pairMatrix = [
-            ['FPSOffloading', 'FPSLocal'],
-            ['serverTimeOffloading', 'slamTimeLocal'],
-            ['renderTimeOffloading', 'renderTimeLocal'],
-            ['videoTimeOffloading', 'videoTimeLocal']
+    graphs = [
+            'FPS',
+            'slamTime',
+            'renderTime',
+            'segmentationTime'
             ]
     
     
@@ -187,14 +199,11 @@ def main():
         
         offloading_data = gen_formated_data(offloading_statistic_path, offloading_labels)
         local_data = gen_formated_data(local_statistic_path, local_labels)
-        
-        gen_bar_plot(statistic, offloading_data, 'offloading', {'slamTimeOffloading': 'Slam', 'renderTimeOffloading':'Render','videoTimeOffloading':'Video', 'streamingTimeOffloading':'Network'})
-        gen_bar_plot(statistic, local_data, 'local', {'slamTimeLocal': 'Slam', 'renderTimeLocal':'Render','videoTimeLocal':'Video'})
+        gen_bar_plot(statistic, offloading_data, 'offloading', {'slamTime': 'Slam', 'renderTime':'Render','segmentationTime':'Segmentation', 'networkTime':'Network'})
+        gen_bar_plot(statistic, local_data, 'local', {'slamTime': 'Slam', 'renderTime':'Render','segmentationTime':'Segmentation'})
 
-        for pair in pairMatrix:
-            gen_pair_graphs(pair, offloading_data, local_data, statistic)        
-        
-
+        for graph in graphs:
+            gen_pair_graphs(graph, offloading_data, local_data, statistic)        
         
 if __name__ == "__main__":
     main()
