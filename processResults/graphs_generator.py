6976dc31e8e4e1534f9116dba80805d04099cdac
xxGabrielNeryxx <gabrielgildino@gmail.com>
2023-12-12 12:53:47 -0300

    confidence intervals

--- processResults/graphs_generator.py
+++ processResults/graphs_generator.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
 import math
-
     
-def gen_formated_data(offloading_statistics_path, labels): 
+def gen_formated_data(statistics_path, no_tc ,labels): 
     cases = {}
     z= 1.96
     
-    for case in os.listdir(offloading_statistics_path):
-        case_path = os.path.join(offloading_statistics_path, case)
+    for case in os.listdir(statistics_path):
+        
+        case_path = os.path.join(statistics_path, case)
         arr = []
         
         for filename in os.listdir(case_path):
@@ -47,7 +47,7 @@ def gen_formated_data(offloading_statistics_path, labels):
             case = case.replace('Mbps', '')    
         cases[float(case)] = data
         
-        
+    cases[0] = no_tc
     cases = dict(sorted(cases.items()))  
     
     return cases
@@ -70,7 +70,7 @@ def gen_bar_plot(statistic, raw_data, case, legend):
     if statistic == 'loss':
         plt.xlabel("Packetloss(%)")
         column_width = 0.5
-        horizontal_line_width=0.5
+        horizontal_line_width=0.1
     
     for key in raw_data.keys():    
         data[key] = raw_data[key].copy(deep= True)
@@ -80,6 +80,16 @@ def gen_bar_plot(statistic, raw_data, case, legend):
     bottom = np.zeros(len(data.keys()))
     
     for label in legend.values():
+        
+        if label == 'Slam':
+            label_color = 'blue'
+        elif label == 'Render':
+            label_color = 'orange'
+        elif label == 'Network':
+            label_color = 'red'
+        elif label == 'Segmentation':
+            label_color = 'green'
+        
         x = list(data.keys())
         y = [data[key][label]['mean'] for key in x]
         y = np.array([0 if math.isnan(item) else item for item in y])
@@ -91,16 +101,16 @@ def gen_bar_plot(statistic, raw_data, case, legend):
 
         
         plt.xticks(x)
-        ax.bar(x, y, label=label, bottom= bottom, width= column_width)
-        
+        ax.bar(x, y , label=label, bottom= bottom, width= column_width, align='center', color = label_color, alpha=0.6)
+        plt.plot(x, y + bottom,  marker="o", markersize=3,linestyle="", alpha=0.8, color=label_color)
         for i in range(len(x)):
-            plt.plot([x[i], x[i]], [upper_limit[i], lower_limit[i]], color = 'black')
-            plt.plot([left[i], right[i]], [upper_limit[i], upper_limit[i]], color = 'black')
-            plt.plot([left[i], right[i]], [lower_limit[i], lower_limit[i]], color = 'black')   
+            plt.plot([x[i], x[i]], [upper_limit[i], lower_limit[i]], color = label_color)
+            plt.plot([left[i], right[i]], [upper_limit[i], upper_limit[i]], color = label_color)
+            plt.plot([left[i], right[i]], [lower_limit[i], lower_limit[i]], color = label_color)   
         
         bottom += y
     
-    plt.ylim([0, max(bottom) + 5])    
+    plt.ylim([0, max(upper_limit) + 5])    
     ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.2f'))
     
     ax.legend() 
@@ -137,8 +147,11 @@ def gen_pair_graphs(graph, offloading_data, local_data, statistic):
     x_offloading = offloading_data.keys()
     y_offloading = [offloading_data[key][graph]['mean'] for key in x_offloading]
     
+    plt.xticks(x_offloading)
+    x_offloading[0] = 'Default'
+    ax.set_xticklabels(x_offloading)
     ax.plot(x_offloading, y_offloading, marker='o', label=legend_offloading, color = offloading_color, linestyle='--')       
-    plt.xticks(list(x_offloading))
+    
     
     for key in x_offloading:
         left = key - horizontal_line_width/2
@@ -155,11 +168,14 @@ def gen_pair_graphs(graph, offloading_data, local_data, statistic):
     local_color = '#2b35af'
     local_legend = 'Local'
     
-    x_local = local_data.keys()
+    x_local = list(local_data.keys())
     y_local = [local_data[key][graph]['mean'] for key in x_local]
     
+    plt.xticks(x_local)
+    x_local[0] = 'Default'
+    ax.set_xticklabels(x_local)
+    
     ax.plot(x_local, y_local, marker='o', label=local_legend, color = local_color, linestyle='--')       
-    plt.xticks(list(x_local))
     
     for key in x_local:
         left = key - horizontal_line_width/2
@@ -186,8 +202,38 @@ def gen_pair_graphs(graph, offloading_data, local_data, statistic):
     elif graph == 'renderTime':
         plt.savefig(f'./graphs/{statistic}_render_time.png')
     elif graph == 'segmentationTime':
-        plt.savefig(f'./graphs/{statistic}_video_time.png')
+        plt.savefig(f'./graphs/{statistic}_segmentation_time.png')
         
+        
+def get_no_tc_data(data_path, labels):
+    z = 1.96
+    arr = []
+    
+    for filename in os.listdir(data_path):
+        df = pd.DataFrame(columns= labels)
+            
+        if filename.endswith('.csv'):
+            file_path = os.path.join(data_path, filename)
+            df = pd.read_csv(file_path)    
+            
+            FPS = (df['totalTime'].count()*1000)/df['totalTime'].sum()
+
+            df = df.mean().to_frame().T
+            df['FPS'] = FPS
+        arr.append(df[labels])
+
+    try:
+        
+        data = pd.concat(arr, ignore_index=True).describe().T
+        data = data[['mean', 'std', 'count']]
+        interval = z * data['std']/np.sqrt(data['count'])
+        data['upper'] = data['mean'] + interval
+        data['lower'] = data['mean'] - interval
+        data = data.T
+        
+    except:
+        data = pd.DataFrame(columns= labels)
+    return data 
 
 def main():
     statistics = ['rate', 'loss', 'delay']
@@ -195,7 +241,6 @@ def main():
     local_path = './dataLocal'
     local_labels = ['totalTime','slamTime', 'renderTime', 'segmentationTime','FPS']
     
-
 
     offloading_path = './dataOffloading'
     offloading_labels = ['totalTime', 'slamTime', 'networkTime', 'renderTime', 'segmentationTime', 'FPS']
@@ -207,14 +252,17 @@ def main():
             'segmentationTime'
             ]
     
-    
-    
+    no_tc_offlaoding_data = get_no_tc_data(os.path.join(offloading_path, 'default'), offloading_labels)
+    no_tc_local_data = get_no_tc_data(os.path.join(local_path, 'default'), local_labels)
+
     for statistic in statistics:
+        
         offloading_statistic_path = os.path.join(offloading_path, statistic)
         local_statistic_path = os.path.join(local_path, statistic)
         
-        offloading_data = gen_formated_data(offloading_statistic_path, offloading_labels)
-        local_data = gen_formated_data(local_statistic_path, local_labels)
+        offloading_data = gen_formated_data(offloading_statistic_path, no_tc_offlaoding_data, offloading_labels)
+        local_data = gen_formated_data(local_statistic_path, no_tc_local_data, local_labels)
+       
         gen_bar_plot(statistic, offloading_data, 'offloading', {'slamTime': 'Slam', 'renderTime':'Render','segmentationTime':'Segmentation', 'networkTime':'Network'})
         gen_bar_plot(statistic, local_data, 'local', {'slamTime': 'Slam', 'renderTime':'Render','segmentationTime':'Segmentation'})
 
