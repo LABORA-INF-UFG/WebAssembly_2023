776eee652ab9689d8450c9e19bc2f73d4c68da05
xxGabrielNeryxx <gabrielgildino@gmail.com>
2023-12-12 07:51:38 -0300

    reading cpu usage

--- tests/powerstats/index.js
+++ tests/powerstats/index.js
@@ -1,11 +1,9 @@
 const process = require('process'); 
 
-
 const puppeteer = require('puppeteer');
 const { spawn } = require('child_process');
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
-const os = require('os');
 const fs = require('fs');
 
   
@@ -14,18 +12,15 @@ async function sleep(time) {
 }
 
 async function measurePowerConsumption() {
-    const usageStart = process.cpuUsage(); 
 
     // Create write stream
 
     const logStream = fs.createWriteStream('test.txt');
 
-    const password = 'sua_senha'; // replace with your password
+    const password = 'helo0714'; // replace with your password
     const command = 'powerstat -d 0 1 -R'; // replace with your command
 
     const powerstat = spawn('sh', ['-c', `echo ${password} | sudo -S ${command}`]);
-
-    
 
     // Pipe powerstat output to log file
     powerstat.stdout.pipe(logStream);
@@ -43,19 +38,7 @@ async function measurePowerConsumption() {
     // Stop powerstat
     await exec('sudo pkill -SIGINT powerstat');
 
-    // Get CPU info
-    const cpus = os.cpus();
-    for(let i = 0; i < cpus.length; i++) {
-        console.log(`CPU ${i}:`);
-        console.log(`User time: ${cpus[i].times.user}`);
-        console.log(`Sys time: ${cpus[i].times.sys}`);
-    }
-
-    // Calling process.cpuUsage() method 
-    const usage = process.cpuUsage(usageStart); 
-  
-    // Printing returned value 
-    console.log(usage); 
+    
 }
 
 measurePowerConsumption();
