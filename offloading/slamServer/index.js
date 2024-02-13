14f8d8caca9a6a5f2c8b8ff35709ce8d4b742159
xxGabrielNeryxx <gabrielgildino@gmail.com>
2024-02-13 17:46:45 -0300

    fixing multithreading

--- offloading/slamServer/index.js
+++ offloading/slamServer/index.js
@@ -1,66 +1,51 @@
-import { createServer } from 'node:http';
-import { Server } from 'socket.io';
-import { Worker } from 'worker_threads';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import { Worker } from "worker_threads";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const port = 3000;
 
-const server = createServer()
-  .listen(port, () =>
+const server = createServer().listen(port, () =>
     console.log(`Server running on port ${port}`)
-  );
+);
 
 const sockets = new Server(server, {
-  cors: {
-    origin: "*"
-  },
-  maxHttpBufferSize: 20 * 1024 * 1024
+    cors: {
+        origin: "*",
+    },
+    maxHttpBufferSize: 20 * 1024 * 1024,
 });
 
-sockets.on('connection', (socket) => {
-  let worker;
+sockets.on("connection", (socket) => {
+    let worker;
 
-  socket.on('initialize alva', async (dimensions, callback) => {
-    const { width, height } = dimensions;
-    worker = new Worker(__dirname + '/worker.js', {
-      workerData: { width, height }
+    socket.on("initialize alva", async (dimensions, callback) => {
+        const { width, height } = dimensions;
+        worker = new Worker(__dirname + "/worker.js", {
+            workerData: { width, height },
+        });
+        callback();
     });
-    callback();
-  });
 
-  socket.on('frame', async (frame, callback) => {
-    let threadStart;
-    
-    worker.once('message', (message) => {
-      const threadEnd = performance.now();
-      // console.log(`threadEnd: ${threadEnd}ms`);
+    socket.on("frame", async (frame, callback) => {
+        let threadStart;
 
-      // const slamTime = message[1];
-      
-      // const threadTime = (threadEnd - threadStart - slamTime).toFixed(2);
-      
-      // console.log(`threadTime: ${threadTime}ms`);
-      // console.log(`slamTime: ${slamTime}ms`);
-      
-      callback([undefined, message])
+        worker.once("message", (message) => {
+            const threadEnd = performance.now();
+
+            const data = message[0];
+            const slamTime = message[1];
+
+            const threadTime = (threadEnd - threadStart - slamTime).toFixed(2);
+            console.log(threadTime);
+            callback([data, slamTime]);
+        });
+
+        threadStart = performance.now();
+
+        worker.postMessage(frame);
     });
-    
-    // const memory = new SharedArrayBuffer(1024 * 1024 * 100)
-    const arr = new Uint8ClampedArray(frame.data);
-    
-    // for(let i = 0; i < frame.data.byteLength; i++) {
-    //   arr[i] = frame.data[i]
-    
-    // }
-    // console.log(...b)
-    
-    // worker.postMessage(b, [b.buffer]);
-
-    threadStart = performance.now()
-    // console.log(`threadStart: ${threadStart}ms`);
-    worker.postMessage(arr)
-  });
 });
--- offloading/slamServer/worker.js
+++ offloading/slamServer/worker.js
@@ -1,46 +1,23 @@
-import { workerData, parentPort } from 'worker_threads';
-import { AlvaAR } from './alva_ar.js';
+import { workerData, parentPort } from "worker_threads";
+import { AlvaAR } from "./alva_ar.js";
 
 const { width, height } = workerData;
 
 const alva = await AlvaAR.Initialize(width, height);
 
+parentPort.on("message", async (message) => {
+    const start = performance.now();
 
-parentPort.on('message', async (message) => {
-  // console.log(`threadArrive: ${start}ms`);
-  
-  
-  // console.log(message[1023])
-  // const frameData = {
-  //   width,
-  //   height,
-  //   data: message
-  // };
-  
-  // console.log("aqui")
-  
-  const start = performance.now();
+    const pose = alva.findCameraPose(message);
+    const planePose = alva.findPlane();
+    const dots = alva.getFramePoints();
+    const end = performance.now();
 
-  const pose = alva.findCameraPose(message);
-  const planePose = alva.findPlane();
-  const dots = alva.getFramePoints();
+    const data = {
+        pose: pose ? pose : null,
+        planePose: planePose ? planePose : null,
+        dots: dots,
+    };
 
-  const end = performance.now();
-  
-  // console.log(frameData)
-  
-  // const dat = {
-  //   pose: pose ? pose : null,
-  //   planePose: planePose ? planePose : null,
-  //   dots: dots,
-  // };
-  
-  // console.log(data);
-  
-  // console.log(`threadSend: ${end}ms`);
-
-  // const poseBuffer = new Float32Array(pose)
-
-  // console.log(pose)
-  parentPort.postMessage(end - start);
-})
+    parentPort.postMessage([data, end - start]);
+});
