f2487b84e665fa86d6371bb6c3d8216b06647314
Matheus Lucas <nerdola.cartola@gmail.com>
2024-03-21 10:31:45 -0300

    Adding compression through dataUrl

--- offloading/slamServer/slam.js
+++ offloading/slamServer/slam.js
@@ -2,6 +2,8 @@ import { workerData, parentPort, Worker } from "worker_threads";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { AlvaAR } from "./alva_ar.js";
+import { createCanvas, Image } from 'canvas';
+import { Blob } from 'node:buffer';
 
 const { width, height } = workerData;
 
@@ -9,21 +11,75 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const alva = await AlvaAR.Initialize(width, height);
 const sender = new Worker(__dirname + "/sender.js")
+const canvas = createCanvas(width, height)
+
+async function dataUrlToImageData(dataUrl) {
+    if (dataUrl === undefined || dataUrl === null) {
+        return;
+    }
+
+    const context = canvas.getContext('2d');
+    const image = new Image();
+
+    return new Promise(resolve => {
+        image.onload = () => {
+            context.drawImage(image, 0, 0, width, height);
+            const imageData = context.getImageData(0, 0, width, height);
+            resolve(imageData);
+        }
+
+        image.src = dataUrl;
+    })
+}
+
+function imageDataToDataUrl(imageData) {
+    const ctx = canvas.getContext('2d');
+    canvas.width = imageData.width;
+    canvas.height = imageData.height;
+    ctx.putImageData(imageData, 0, 0);
+
+    const img = new Image();
+    img.src = canvas.toDataURL();
+    return img;
+}
+
+async function blobToImageData(blob) {
+    const canvas = createCanvas(width, height)
+
+    const url = URL.createObjectURL(blob);         // create an Object URL
+    const img = new Image();                       // create a temp. image object
+    let context = null;
+
+    return new Promise(resolve => {
+        img.onload = () => { // handle async image loading
+            console.log('aqui')
+            URL.revokeObjectURL(img.src); // free memory held by Object URL
+            context = canvas.getContext("2d")
+            context.drawImage(img, 0, 0); // draw image onto canvas (lazy method™)
+            const imageData = context.getImageData(0, 0, width, height);
+            resolve(imageData);
+        };
+
+        img.src = url;
+    })
+
+}
 
 parentPort.on("message", (message) => makeSLAM(message))
 
-function makeSLAM(message) {
-    // console.log('start make slam - ' + performance.now().toFixed(2))
+async function makeSLAM(message) {
+    // const blob = new Blob(message.blob, {type: 'image/png'});
+    // const a = await blobToImageData(blob);
+
+    const { dataUrl } = message;
+    const b = await dataUrlToImageData(dataUrl);
 
     const start = performance.now();
-    // console.log('start - ' + start)
-
-    const pose = alva.findCameraPose(message);
+    const pose = alva.findCameraPose(b);
     const planePose = alva.findPlane();
     const dots = alva.getFramePoints();
     const end = performance.now();
 
-    // console.log('end - ' + end)
 
     const data = {
         pose: pose ? pose : null,
@@ -31,14 +87,13 @@ function makeSLAM(message) {
         dots: dots,
     };
 
-    const {data: frame} = message;
+    const { data: frame } = message;
 
     sender.postMessage({
         frame,
+        dataUrl,
         ...message,
-        data, 
-        totalSlamTime: end - start, 
+        data,
+        totalSlamTime: end - start,
     });
-    // console.log("processei frame - " + message.frameIndex);
-    // console.log('end make slam - ' + performance.now().toFixed(2))
 }
