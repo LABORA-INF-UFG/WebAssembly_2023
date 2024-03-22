30345e8f824943d48bd2ffb30c6c7532b82f2dcc
Matheus Lucas <nerdola.cartola@gmail.com>
2024-03-22 12:12:03 -0300

    Adding wasm-flate compression

--- offloading/staticServer/public/scripts/utils.js
+++ offloading/staticServer/public/scripts/utils.js
@@ -68,6 +68,40 @@ function onFrame(frameTickFn, fps = 30) {
     requestAnimationFrame(onAnimationFrame);
 }
 
+function roughSizeOfObject(object) {
+    const objectList = [];
+    const stack = [object];
+    let bytes = 0;
+  
+    while (stack.length) {
+      const value = stack.pop();
+  
+      switch (typeof value) {
+        case 'boolean':
+          bytes += 4;
+          break;
+        case 'string':
+          bytes += value.length * 2;
+          break;
+        case 'number':
+          bytes += 8;
+          break;
+        case 'object':
+          if (!objectList.includes(value)) {
+            objectList.push(value);
+            for (const prop in value) {
+              if (value.hasOwnProperty(prop)) {
+                stack.push(value[prop]);
+              }
+            }
+          }
+          break;
+      }
+    }
+  
+    return bytes;
+  }
+
 function isIOS() {
     return /iPad|iPhone|iPod/.test(navigator.platform);
 }
@@ -334,4 +368,4 @@ class Video {
     }
 }
 
-export { Camera, Video, onFrame, isMobile, isIOS, getScreenOrientation, resize2cover, rad2deg, deg2rad, getCSV, Queue }
+export { Camera, Video, onFrame, isMobile, isIOS, getScreenOrientation, resize2cover, rad2deg, deg2rad, getCSV, Queue, roughSizeOfObject }
